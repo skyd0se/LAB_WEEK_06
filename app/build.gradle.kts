19f5065388c06623ede3a8b2d500b4540bc73461
skyd0se <fitzpatrick.rinsen@gmail.com>
2025-10-03 12:13:30 +0700

    Commit No.1 - Adding a RecyclerView and Populating it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,14 @@ android {
 }
 
 dependencies {
+    implementation("com.github.bumptech.glide:glide:4.14.2")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.filament.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
